8d31c2952eb7657d426437741b5f908d5a711bf9
fcvz <fcvz@users.noreply.github.com>
2018-12-14 00:27:33 +0100

    Commit test

--- STM32F746G2_LD1_Button/Src/main.c
+++ STM32F746G2_LD1_Button/Src/main.c
@@ -104,19 +104,10 @@ int main(void) {
    /* Infinite loop */
    /* USER CODE BEGIN WHILE */
    while (1) {
-
       /* USER CODE END WHILE */
-
       /* USER CODE BEGIN 3 */
       HAL_GPIO_TogglePin( GPIOI, GPIO_PIN_1);
       HAL_Delay(tDelay);
-
-
-
-
-
-
-
    }
    /* USER CODE END 3 */
 
